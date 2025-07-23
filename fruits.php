73cd9ec334b16d52073fbbcdbb1990aa4c89c147
yokodori <yokodori4@gmail.com>
2025-07-23 15:31:08 +0900

    # feat:出荷後の在庫状況報告を追加実装
    ## 概要
    出荷完了後、在庫状況を確認して報告する仕様を実装しました。
    
    ## 詳細
    - 在庫状況は`var_export`で出力
    - 在庫数が0だった場合は、在庫終了を伝える
    - 出荷した果物がリストから削除される仕組みは未実装

--- fruits.php
+++ fruits.php
@@ -3,17 +3,40 @@ $fruits = [0 => "りんご", 1 => "みかん", 2 => "ぶどう"];
 $count = count($fruits);
 
 function export_fruits ($number){
+    $type = is_int($number);
     global $count;
     global $fruits;
-    if ($number <= $count){
-        echo "これから果物を{$number}個、出荷します！\n";
-        for ($i = 0; $i < $number; $i++){
-            echo "{$fruits[$i]}を出荷！\n";
+    if ($type == 1) { 
+        if ($number <= $count){
+            echo "これから果物を{$number}個、出荷します！\n";
+            for ($i = 0; $i < $number; $i++){
+                echo "{$fruits[0]}を出荷！\n";
+                array_shift($fruits);
+            }
+            echo "すべての出荷が完了しました。\n";
+            $count = count($fruits);
+            if ($count === 0){
+                echo "これで在庫はもうありません。\n";
+            }else{
+                echo "残りの在庫状況は以下のとおりです。\n";
+                var_export($fruits);
+            }
+        }else{
+            echo "指定した数量が在庫数を超えています。\n";
         }
-        echo "すべての出荷が完了しました。\n";
     }else{
-        echo "指定した数量が在庫数を超えています。\n";
+        echo "エラー：整数の値を入力してください。\n";
     }
 }
-export_fruits(3)
+// 試行1回目:在庫3つのうち、2つを出荷
+echo "--- 試行1回目です！ ---\n";
+export_fruits(2);
+
+//試行2回目:残りの1つを出荷
+echo "--- 試行2回目です！ ---\n";
+export_fruits(1);
+
+//試行3回目:在庫切れでエラー
+echo "--- 試行3回目です！ ---\n";
+export_fruits(2);
 ?>
